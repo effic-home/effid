6481e1b3531aad42ef8cbf3eff9a54498c2d2506
Ventouz <potoroo725@gmail.com>
2020-08-08 16:55:43 +0200

    wesh y'en a marre je tente un truc

--- install.sh
+++ install.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
+
+### Mise a jour et installation utilitaires de base
 apt update -y
 apt upgrade -y
 apt install snmpd git -y
 apt install apt-transport-https ca-certificates software-properties-common -y
 
-## Installer Docker
+### Installer Docker
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 curl https://download.docker.com/linux/raspbian/gpg
 echo "deb https://download.docker.com/linux/raspbian/ stretch stable" > /etc/apt/sources.list
 apt-get update
 apt upgrade -y
 
-## Lancement automatique Docker
+### Lancement automatique Service Docker
 systemctl start docker.service
 systemctl enable docker.service
 docker info
 
-## Docker Compose
+## Installation Docker Compose
 apt install -y libffi-dev libssl-dev
 apt install -y python3 python3-pip
 pip3 install docker-compose
+
+### Installer cluster
+git clone https://github.com/effid/effi-API API
+git clone https://github.com/effid/effid-sql SQL
+git clone https://github.com/effid/effid-web WEB
+
+docker-compose up -d
