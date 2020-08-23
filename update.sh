3049b1feaf07c038b5a58c90b5e2c57251741c34
Ventouz <potoroo725@gmail.com>
2020-08-23 18:13:40 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+docker rmi effid_web:latest
+docker rmi effid_api:latest
+
 ### Update cluster
 git pull -v
+
 cd API
 git pull -v
 cd ../SQL
@@ -9,4 +13,6 @@ git pull -v
 cd ../WEB
 git pull -v
 
+cd ..
 docker-compose build
+docker-compose up -d
